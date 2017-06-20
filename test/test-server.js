345c059b806ab62904c47db734b9fbed13e4e637
Edgar <exgc12@gmail.com>
2017-06-20 15:58:30 -0400

    Added POST test

---
+++ test/test-server.js
@@ -0,0 +1,52 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Blog API', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should return all blog posts on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.be.at.least(1);
+
+        const expectedKeys = ['id', 'title', 'content', 'author'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it('should create new blog post on POST', function() {
+    const newItem = {title: 'some title', content: 'some content', author: 'some author'};
+    return chai.request(app)
+    .post('/blog-posts')
+    .send(newItem)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('id', 'title', 'content', 'author');
+      res.body.should.not.be.null;
+      res.body.id.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+    });
+  });
+
+})
